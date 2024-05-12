77517ce75204490cb5f607ba171150cc6cc9974a
Eric <ekellogg90@gmail.com>
2024-05-12 01:18:59 -0500

    cleaning up script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,6 @@ let species = "dog"; // we'll need to make this dynamic later - eric
 responseForm.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit() {
-    // const speciesAnswer = speciesInput.value;
-    // const petAgeAnswer = ageInput.value;
-    // const petGenderAnswer = genderInput.value;
-    // const petDistanceAnswer = distanceInput.value;
-    // const locationAnswer = locationInput.value.trim();
-    
-    // This checks if each field is filled out before allowing submission to occur.
-
-
     const petInfo = {
         species: speciesInput.value,
         age: ageInput.value,
@@ -49,17 +40,14 @@ function handleFormSubmit() {
 
     // TODO push petInfo to local storage
     getAdoptPetData(petInfo);
-    //location.href = "results.html";
+    //location.href = "results.html";  // have this commented out for the moment to more easily see console log of the index page
     
 }
 
 // Calls the adopt a pet API and retrieves data on the selected species
 function getAdoptPetData(petInfo) {
     console.log(petInfo);
-   
-    // I commented out the first petUrl line for now, but once we get local storage working I think that's the one we'll use. -eric
     const petUrl = "https://api-staging.adoptapet.com/search/pet_search?key="+petApiKey+"&v=3&output=json&city_or_zip="+petInfo.location+"&geo_range="+petInfo.distance+"&species="+petInfo.species+"&breed_id=real%3D801&sex="+petInfo.gender+"&age="+petInfo.age+"&start_number=1&end_number=50";
-    //const petUrl = "https://api-staging.adoptapet.com/search/pet_search?key="+petApiKey+"&v=3&output=json&city_or_zip=47374&geo_range=50&species=dog";  // this will be deleted in favor of the one above once LS setup
 
     fetch(petUrl)
     .then(function (response) {
@@ -76,5 +64,3 @@ function getAdoptPetData(petInfo) {
         alert('Unable to connect to Adopt a Pet API');
     });
 };
-
-//getAdoptPetData(); // have this just to test that the function is providing data.  We can delete this line later - eric
