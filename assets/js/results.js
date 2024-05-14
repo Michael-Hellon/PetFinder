c775532c7ee35238e6abadfca7fea838261a0319
Michael Hellon <kansasboy308@gmail.com>
2024-05-14 16:54:02 -0500

    working on render
    Changes to be committed:
    modified:   assets/js/results.js
    modified:   assets/js/snippets.js
    modified:   results.html

--- assets/js/results.js
+++ assets/js/results.js
@@ -2,6 +2,17 @@ const backBtn = document.querySelector('#back-button');
 const resultContainer = document.querySelector('#result-container');
 const searchResultEl = document.getElementById("results")
 
+// console.log('localStorage pet_name length', localStorage.pet_name.length) - YES IT WORKS
+// console.log('localStorage petData', localStorage.petData) - YES, ITs a mess
+// console.log('petArray', petArray) - NOPE
+// console.log('petData', petData) - NOPE
+
+let retrievedPetString = localStorage.getItem("petArray");
+let petArray = JSON.parse(retrievedPetString);
+// console.log('petData', petData);
+console.log('petArray', petArray);
+console.log('petAge', petArray[1].name);
+console.log("petArray.length", petArray.length);
 
 
 // Back Button for Results Page
@@ -9,54 +20,81 @@ backBtn.addEventListener('click', function() {
     location.href = "index.html"
 });
 
-function getPetResultsFromLocalStorage() {
-    return JSON.parse(localStorage.getItem(petArray)) || []
+function getPetResultsFromLocalStorage(petArray) {
+    JSON.parse(localStorage.getItem())
 
+    console.log('pet Array', petArray)
+    for( let i = 0; i < petArray.length; i++) {
 
-    for( let i = 0; i < data.pets.length; i++) {
-        let petAge = data.pets[i].age;
-        let petName = data.pets[i].pet_name;
-        let petGender = data.pets[i].sex;
-        let petBreed= data.pets[i].primary_breed;
-        let petPic = data.pets[i].large_results_photo_url;
+        let petAge = petArray[i].age;
+        console.log("Hello");
+        let petName = petArray[i].name;
+        let petGender = petArray[i].sex;
+        // let petBreed= petArray[i].breed;
+        let petPic = petArray[i].pic;
+        let petDetails = petArray[i].details;
 
-        let newDiv = document.createElement("div");
-        let newFig = document.createElement("figure")
-        let newImg = document.createElement("img");
-        let newDiv2 = document.createElement("div");
-        let newDiv3 = document.createElement("div");
-        let newDiv4 = document.createElement("div");
-        let newDiv5 = document.createElement("div");
-        let newH2 = document.createElement("h2");
-        let newP = document.createElement("p");
+        
+        console.log('petAge', petArray[i].age);
+        console.log('name', petArray[i].name);
+        console.log('sex', petArray[i].sex);
+        console.log("pet pic", petArray[i].pic); 
+        console.log("pet Detail", petArray[i].details);     
+        console.log("petArray.length",petArray[i].length);
 
-        newDiv.className = "card w-64 bg-base-100 shadow-xl";
-
-        newImg.src =  `https://pet-uploads-staging.adoptapet.com/${petPic}.jpg`;
-        newImg.alt = "Pet Picture"
-
-        newDiv2.className = "card-body";
-
-        newH2.className = "card-title";
-        newH2.innerHTML = `${petName}`
-
-        newP.innerHtml = `${petDetails}`;
-
-        newDiv3.className = "card-actions justify-end"
-        newDiv4.innerHTML = 'class="badge badge-outline">Add To Favorites';
-        newDiv5.innerHTML ='class="badge badge-outline">Pick Me!';
-
-
-        newFig.append(newImg);
-        newDiv.append(newFig);
-        newDiv3.append(newDiv4);
-        newDiv3.append(newDiv5);
-        newDiv2.append(newDiv3);
-        newDiv.append(newDiv2);
-
-        searchResultEl.append(newDiv);
+        resultContainer.innerHTML += String.raw`
+        <div class="card w-64 bg-base-100 shadow-xl">
+          <figure><img src=${petArray.pic ? petArray.pic : "No Picture Available"} alt="pet"/></figure>
+          <div class="card-body">
+            <h2 class="card-title">${petArray.pet_name ? petArray[i].name : "No Picture Available"}
+                          
+            </h2>
+            <p> `${petArray[i].details}``</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Add To Favorites</div> 
+              <div class="badge badge-outline">Pick Me!</div>
+            </div>
+          </div>
+        </div>`;
     }
-}
+  }
+
+//         let newDiv = document.createElement("div");
+//         let newFig = document.createElement("figure")
+//         let newImg = document.createElement("img");
+//         let newDiv2 = document.createElement("div");
+//         let newDiv3 = document.createElement("div");
+//         let newDiv4 = document.createElement("div");
+//         let newDiv5 = document.createElement("div");
+//         let newH2 = document.createElement("h2");
+//         let newP = document.createElement("p");
+
+//         newDiv.className = "card w-64 bg-base-100 shadow-xl";
+
+//         newImg.alt = "Pet Picture"
+
+//         newDiv2.className = "card-body";
+
+//         newH2.className = "card-title";
+//         newH2.innerHTML = `${petName}, sex: ${petGender}, Pet's Age: ${petAge}`
+
+//         newP.innerHtml = `${petDetails}`;
+
+//         newDiv3.className = "card-actions justify-end"
+//         newDiv4.innerHTML = 'class="badge badge-outline">Add To Favorites';
+//         newDiv5.innerHTML ='class="badge badge-outline">Pick Me!';
 
 
-    console.log('petArray returned', petArray)
+//         newFig.append(newImg);
+//         newDiv.append(newFig);
+//         newDiv3.append(newDiv4);
+//         newDiv3.append(newDiv5);
+//         newDiv2.append(newDiv3);
+//         newDiv.append(newDiv2);
+
+//         searchResultEl.append(newDiv);
+//     }
+// }
+
+
+    // console.log('petArray returned', petArray)
--- assets/js/snippets.js
+++ assets/js/snippets.js
@@ -1,17 +1,38 @@
-/*  code Snippets   -   Can delete once done with project
+// /*  code Snippets   -   Can delete once done with project
 
                 
-                localStorage.setItem(petName, JSON.stringify(petArray));
+//                 localStorage.setItem(petName, JSON.stringify(petArray));
 
 
-                petAge = data.pets[1].age;
-                petName = data.pets[1].pet_name;
-                petGender = data.pets[1].sex;
-                petBreed= data.pets[1].primary_breed;
-                petPic = data.pets[1].large_results_photo_url;
-                petDetails = data.pets[0].details_url;
+//                 petAge = data.pets[1].age;
+//                 petName = data.pets[1].pet_name;
+//                 petGender = data.pets[1].sex;
+//                 petBreed= data.pets[1].primary_breed;
+//                 petPic = data.pets[1].large_results_photo_url;
+//                 petDetails = data.pets[0].details_url;
 
-                petArray.push(petAge, petName, petGender, petBreed, petPic, petDetails);                
-                localStorage.setItem(petName, JSON.stringify(petArray));
+//                 petArray.push(petAge, petName, petGender, petBreed, petPic, petDetails);                
+//                 localStorage.setItem(petName, JSON.stringify(petArray));
 
-*/
+// */
+// console.log('petAge', petAge);
+//         let petName = petArray[i].pet_name;
+//         let petGender = petArray[i].sex;
+//         let petBreed= petArray[i].breed;
+//         let petPic = petArray[i].pic;
+//         let petDetails = petArray[i].details;
+
+//         <div class="card w-64 bg-base-100 shadow-xl">
+//           <figure>img=`${petPic}` alt=`${petName}`></figure>
+//           <div class="card-body">
+//             <h2 class="card-title">`${petName}`
+//               <div class="badge badge-secondary">NEW</div>
+            
+//             </h2>
+//             <p>Loving fun dog that loves long walks in the Park, and loves to go hiking. </p>
+//             <div class="card-actions justify-end">
+//               <div class="badge badge-outline">Add To Favorites</div> 
+//               <div class="badge badge-outline">Pick Me!</div>
+//             </div>
+//           </div>
+//         </div>
