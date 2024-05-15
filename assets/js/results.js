5fd5e833fc694062aef60692bec3bf064c2c1360
Eric <ekellogg90@gmail.com>
2024-05-14 20:43:10 -0500

    fixed script redirect and cleaned up some appends

--- assets/js/results.js
+++ assets/js/results.js
@@ -24,14 +24,14 @@ function getPetResultsFromLocalStorage() {
         const newDiv1El = document.createElement("div");
         newDiv1El.className = "card w-64 bg-base-100 shadow-xl";
 
-        const newFigEl = document.createElement("figure");
+        //const newFigEl = document.createElement("figure");
+
+        // const newDiv2El = document.createElement("div")
+        // newDiv2El.className = "card-body";
 
         const petPicEl = document.createElement("img");
         let petPic = petArray[i].pic;
         petPicEl.src = petPic;
-
-        const newDiv2El = document.createElement("div")
-        newDiv2El.className = "card-body";
 
         const petNameEl = document.createElement("h3");
         let petName = petArray[i].name;
@@ -41,30 +41,34 @@ function getPetResultsFromLocalStorage() {
         const petDetailsEl = document.createElement("p");
         let petDetails = petArray[i].details;
         petDetailsEl.textContent = petDetails;
+
+        newDiv1El.appendChild(petNameEl);
+        newDiv1El.appendChild(petPicEl);
+        newDiv1El.appendChild(petDetailsEl);
        
 
-        const newDiv3El = document.createElement("div");
-        newDiv3El.className = "card-actions justify-end";
+        // const newDiv3El = document.createElement("div");
+        // newDiv3El.className = "card-actions justify-end";
 
-        const newDiv4El = document.createElement("div");
-        newDiv4El.className = "badge badge-outline";
-        let contactEmail = petArray[i].email;
-        newDiv4El.textContent = contactEmail;
+        // const newDiv4El = document.createElement("div");
+        // newDiv4El.className = "badge badge-outline";
+        // let contactEmail = petArray[i].email;
+        // newDiv4El.textContent = contactEmail;
 
-        const newDiv5El = document.createElement("div");
-        newDiv5El.className = "badge badge-outline";
+        // const newDiv5El = document.createElement("div");
+        // newDiv5El.className = "badge badge-outline";
         // newDiv5El.attrteibutes = Pick Me!
 
-        newFigEl.appendChild(petPicEl); // good
-        petCardEl.appendChild(newFigEl);
-        newDiv2El.appendChild(petDetailsEl);
-        newDiv2El.appendChild(petNameEl);
+        //newFigEl.appendChild(petPicEl); // good
+        //petCardEl.appendChild(newFigEl);
+        // newDiv2El.appendChild(petDetailsEl);
+        // newDiv2El.appendChild(petNameEl);
 
-        newDiv3El.appendChild(newDiv4El);  // good
-        newDiv3El.appendChild(newDiv5El);  // good 
-        newDiv2El.appendChild(newDiv3El);  // good
-        newDiv1El.appendChild(newDiv2El);  //good
-        petCardEl.appendChild(newDiv1El);
+        // newDiv3El.appendChild(newDiv4El);  // good
+        // newDiv3El.appendChild(newDiv5El);  // good 
+        // newDiv2El.appendChild(newDiv3El);  // good
+        // newDiv1El.appendChild(newDiv2El);  //good
+        // petCardEl.appendChild(newDiv1El);
         
 
 
@@ -72,7 +76,7 @@ function getPetResultsFromLocalStorage() {
         // petCardEl.appendChild(petNameEl);
         
         // appends it all together
-        petList.appendChild(petCardEl);
+        petList.appendChild(newDiv1El);
 
     }
     };
