e7d13f823bd6eb388f2a8ec3866e50089f41d36e
Eric <ekellogg90@gmail.com>
2024-05-16 18:51:30 -0500

    changed map back for demo

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,12 @@
 
 const geoArray = JSON.parse(localStorage.getItem('geoArray'));
-// geoArrayLat = geoArray[geoArray.length - 1].latitude;
-// geoArrayLon = geoArray[geoArray.length - 1].longitude;
+geoArrayLat = geoArray[geoArray.length - 1].latitude;
+geoArrayLon = geoArray[geoArray.length - 1].longitude;
 
 map = new OpenLayers.Map("mapdiv");
 map.addLayer(new OpenLayers.Layer.OSM());
 
-//var lonLat = new OpenLayers.LonLat(geoArrayLon ,geoArrayLat)
-var lonLat = new OpenLayers.LonLat( -94.5 ,39.09 )
+var lonLat = new OpenLayers.LonLat(geoArrayLon ,geoArrayLat)
 .transform(
       new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
       map.getProjectionObject() // to Spherical Mercator Projection
